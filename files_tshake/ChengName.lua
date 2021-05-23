c7c896331964e30d8c0c5753e7a79a9f51f41892
HyDrASM <84361933+HyDrASM@users.noreply.github.com>
2021-05-23 23:36:29 +0300

    Update ChengName.lua

--- files_tshake/ChengName.lua
+++ files_tshake/ChengName.lua
@@ -1,19 +1,19 @@
 
-local function Tshakee(msg)
+local function Hydraa(msg)
 local text = msg.content_.text_
 if text then 
 tdcli_function({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,data)
 if data.id_ then 
 if data.id_ ~= bot_id then
-local TshakeChengName = database:get(bot_id.."Tshake:Cheng:Name"..data.id_)
+local HydraChengName = database:get(bot_id.."Hydra:Cheng:Name"..data.id_)
 if not data.first_name_ then 
-if TshakeChengName then 
-send(msg.chat_id_, msg.id_, "يولد هذا غير اسمه لك ها كلب  ["..TshakeChengName..']')
-database:del(bot_id.."Tshake:Cheng:Name"..data.id_) 
+if HydraChengName then 
+send(msg.chat_id_, msg.id_, "يولد هذا غير اسمه لك ها كلب  ["..HydraChengName..']')
+database:del(bot_id.."Hydra:Cheng:Name"..data.id_) 
 end
 end
 if data.first_name_ then 
-if TshakeChengName ~= data.first_name_ then 
+if HydraChengName ~= data.first_name_ then 
 local Text = {
   ' هاحبي شكو غيرت اسمك 🌞😹😹😹',
   "هاحبي بعبصوك وغيرت اسمك 🤞😂😂",
@@ -23,7 +23,7 @@ local Text = {
 }
 send(msg.chat_id_, msg.id_,Text[math.random(#Text)])
 end  
-database:set(bot_id.."Tshake:Cheng:Name"..data.id_, data.first_name_) 
+database:set(bot_id.."Hydra:Cheng:Name"..data.id_, data.first_name_) 
 end
 end
 end
@@ -31,4 +31,4 @@ end,nil)
 end
 
 end
-return {Tshake = Tshakee}
+return {Hydra = Hydraa}
